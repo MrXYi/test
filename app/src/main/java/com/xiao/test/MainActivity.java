13769b0a02d7fc4a928869498b46debdcc78cb8a
xiaoyi <919118325@qq.com>
2018-09-13 15:51:35 +0800

    import

--- app/src/main/java/com/xiao/test/MainActivity.java
+++ app/src/main/java/com/xiao/test/MainActivity.java
@@ -3,6 +3,7 @@ package com.xiao.test;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
